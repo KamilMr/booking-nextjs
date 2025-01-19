6ac2436fa29e3e8926296192ee936ed91b519f6f
kamil <kamil@chicos.life>
2025-01-19 19:19:41 +0100

    Refactor Book page component for improved readability and structure

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,11 +1,11 @@
-"use client";
-import { useState } from "react";
-import clsx from "clsx";
-import { Button } from "../ui/button";
-import { FirstStep } from "@/app/ui/book/first-step";
-import { Paper } from "@/app/ui/paper";
-import { SecondScreen } from "@/app/ui/book/second-screen";
-import { AvatarNameSection } from "@/app/ui/avatar-name-section";
+'use client';
+import {useState} from 'react';
+import clsx from 'clsx';
+import {Button} from '../ui/button';
+import {FirstStep} from '@/app/ui/book/first-step';
+import {Paper} from '@/app/ui/paper';
+import {SecondScreen} from '@/app/ui/book/second-screen';
+import {AvatarNameSection} from '@/app/ui/book/avatar-name-section';
 
 interface Form {
   specialist: string;
@@ -17,36 +17,45 @@ interface Form {
 }
 
 const initState = () => ({
-  specialist: "Amy Burns",
-  location: "",
-  type: "",
-  date: "",
-  name: "",
-  phone: "",
+  specialist: 'Amy Burns',
+  location: '',
+  type: '',
+  date: '',
+  name: '',
+  phone: '',
 });
+
+const allSteps = ['location', 'type', 'date', 'client', 'summary'];
+const plTranslation = {
+  location: 'Miejsce spotkania',
+  type: 'Typ spotkania',
+  date: 'Data spotkania',
+  client: 'Dane klienta',
+  summary: 'Podsumowanie',
+}
 
 export default function Page() {
   const locations = [
-    { id: 1, name: "Location 1" },
-    { id: 2, name: "Location 2" },
-    { id: 3, name: "Location 3" },
+    {id: 1, name: 'Location 1'},
+    {id: 2, name: 'Location 2'},
+    {id: 3, name: 'Location 3'},
   ];
   const [page, setPage] = useState<number>(1);
   const [form, setForm] = useState<Form>(initState());
 
-  const retriveData = (form) => {
+  const retriveData = form => {
     const tR: Array<string> = [];
-    Object.keys(form).forEach((key) => {
+    Object.keys(form).forEach(key => {
       if (!form[key]) {
         return [];
       }
       const map = {
-        specialist: "Specjalista",
-        location: "Lokalizacja",
-        type: "Typ spotkania",
-        date: "Data",
-        name: "Imię",
-        phone: "Telefon",
+        specialist: 'Specjalista',
+        location: 'Lokalizacja',
+        type: 'Typ spotkania',
+        date: 'Data',
+        name: 'Imię',
+        phone: 'Telefon',
       };
 
       if (!form[key]) return;
@@ -57,7 +66,7 @@ export default function Page() {
   };
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Paper className="flex flex-col w-full h-full md:w-1/2 md:h-4/5 justify-between">
+      <Paper className="flex flex-col w-full h-full md:w-1/2 md:h-4/5 justify-between relative">
         <AvatarNameSection
           src="/customers/amy-burns.png"
           sectionArr={retriveData(form)}
@@ -66,22 +75,22 @@ export default function Page() {
         {page === 1 && (
           <FirstStep
             locations={locations}
-            onChange={(locationId) => {
+            onChange={locationId => {
               setForm({
                 ...form,
                 // @ts-ignore
-                location: locations.find((l) => l.id === locationId).name,
+                location: locations.find(l => l.id === locationId).name,
               });
             }}
-            value={locations.find((l) => l.name === form.location)?.id ?? null}
+            value={locations.find(l => l.name === form.location)?.id ?? null}
           />
         )}
 
         {page === 2 && (
           <SecondScreen
             meetingType={[
-              { name: "test", value: 1 },
-              { name: "test1", value: 2 },
+              {name: 'test', value: 1},
+              {name: 'test1', value: 2},
             ]}
             onChange={(value: string) => {
               setForm({
@@ -95,14 +104,20 @@ export default function Page() {
         <div className="flex justify-between">
           <Button
             className={clsx(
-              page === 1 ? "opacity-0 pointer-events-none" : "opacity-100",
+              page === 1 ? 'opacity-0 pointer-events-none' : 'opacity-100',
             )}
             onClick={() => setPage(page - 1)}
           >
-            {page === 1 ? "" : "Wstecz"}
+            {page === 1 ? '' : 'Wstecz'}
           </Button>
-          <Button onClick={() => setPage(page + 1)}>Dalej</Button>
+          <Button
+            onClick={() => setPage(page + 1)}
+            disabled={page === allSteps.length}
+          >
+            Dalej
+          </Button>
         </div>
+        <div className="top-0 left-0 absolute">{`${page}/${allSteps.length}`}</div>
       </Paper>
     </div>
   );
